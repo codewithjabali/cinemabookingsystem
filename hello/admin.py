aa7917c461f60c8f16d1b8672bd4539bb8ed6d11
codewithjabali <jabaliamunga@gmail.com>
2025-10-07 11:20:12 +0300

    Update admin.py

--- hello/admin.py
+++ hello/admin.py
@@ -4,8 +4,9 @@ from .models import Customer, Advisor, Booking, perfomance, production
 admin.site.register(Customer)
 admin.site.register(Advisor)
 admin.site.register(Booking)
-admin.site.register(perfomance)
-admin.site.register(production)
+admin.site.register(Perfomance)
+admin.site.register(Production)
 
 
 # Register your models here.
+
